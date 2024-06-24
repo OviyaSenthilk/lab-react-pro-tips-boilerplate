72ca65f26b0dd3b5ff86af10bb2d60f674d919ad
OviyaSenthilk <pji.oviya.s@kalvium.community>
2024-06-24 17:40:40 +0530

    new

---
+++ protips/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import Navbar from './Components/Navbar';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to Pro Tips</h1>
+    </div>
+  );
+};
+
+export default App;
